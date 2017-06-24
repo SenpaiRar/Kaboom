5fa34f16f587a8dc79d751af1da1bacb0e323e83
SenpaiRar <windowsbeb@gmail.com>
2017-06-23 21:35:55 -0400

    Added the criminal and his movement

---
+++ Assets/Scripts/Bomber.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+//Scripts the bomber which moves and drops bombs
+public class Bomber : MonoBehaviour {
+
+    Vector3 NextPoint; //Randomly generated point which the Bomber moves towards
+    public float nMin; //Upper and Lower bound of the x value of NextPoint
+    public float nMax;
+
+    void Start () {
+        NextPoint = GenerateNextPoint(nMin, nMax);
+	}
+	
+	
+	void LateUpdate () {
+        if (Vector3.Distance(transform.position, NextPoint) > 0.5f)
+        {
+            transform.position = Vector3.Lerp(transform.position, NextPoint, 0.2f);
+        }
+        else
+        {
+            NextPoint = GenerateNextPoint(nMin,nMax);
+        }
+
+	}
+
+    Vector3 GenerateNextPoint (float min, float max)
+    {
+        return (new Vector3(Random.Range(min, max), 0, 0));
+    }
+}
---
+++ Assets/Scripts/Bombs.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bombs : MonoBehaviour {
+
+    float speed;
+    float cutoffDistance; //The distance the bomb has to travel before causing a penalty
+
+    public delegate void ExplodeEvent();
+    public static event ExplodeEvent OnExplode;
+    
+	
+	void LateUpdate () {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        if(transform.position.y < transform.position.y - cutoffDistance)
+        {
+            if(OnExplode != null)
+            {
+                OnExplode();
+            }
+        }
+	}
+}
